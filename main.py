02d72df2d4a1c038793c010ad463c872003eadc3
aaron9589 <aaron@cybus.net>
2023-08-22 11:13:38 +1000

    ESP32 support, changed to MQTT Publish, new reliable POST function

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
+# Two key things to optimise code:
+# Put things into functions
+# Assign methods to variables, eg blink=LED.Blink()
+# Add error handling.
 
-import network, time, ujson, socket, json, urequests
+# Try adding error handling from here https://wiki.python.org/moin/HandlingExceptions under 'General Error Catching'
+
+import network, time, ujson, socket, ubinascii, machine
 from machine import WDT
+from umqttsimple import MQTTClient
 
 # Initialise Wifi
 station = network.WLAN(network.STA_IF)
-#station.active(True)
+station.active(True)
 
-
-post = urequests.post
 dumps = ujson.dumps
 sleep = time.sleep
 loads = ujson.loads
 
-wdt = WDT(timeout=60000) # 1 min inactivity
+# Initialise Watchdog
+wdt = WDT(timeout=30000) # 30s inactivity
 
-# SWQ = West array
-# SW25 = East Array
+#Array Wifi SSIDs
+sol = ["Your_Array_SSID", "Your_Array_SSID"]
 
-sol = ["Solax_123456", "Solax_987654"]
+#MQTT Broker Info
+broker = '192.168.0.2'
+client_id = ubinascii.hexlify(machine.unique_id())
+topic = b'/home/solar/espData'
 
 def solax_json():
     wdt.feed()
     try:
-        # Example usage
+        # Set the inverter config - shouldn't change
         host = "5.8.8.8"
         port = 80
         path = "/?optType=ReadRealTimeData"
-
 
         # Create a TCP socket
         client_socket = socket.socket()
@@ -57,25 +65,26 @@ def solax_json():
 
 
         # Print the response
-
         resp = response.decode("utf-8")
 
         # Extract JSON data from response
         json_start = resp.find("{")  # Find the starting index of the JSON data
         json_data = resp[json_start:]  # Extract the JSON portion
-
+        del client_socket
 
         # Print the JSON string
         return json_data
 
     except Exception as error:
-        print("An error occurred:", type(error).__name__)
+        print("An error occurred:", type(error).__name__) 
         return
+
+
 
 def connect_to_inverter(SSID):
     wdt.feed()
     try:
-        station.active(True)
+        #station.active(True)
         #connect to the wifi
         print("connecting to " + SSID)
         station.ifconfig(('5.8.8.10', '255.255.255.0', '5.8.8.8', '5.8.8.8'))
@@ -92,17 +101,19 @@ def connect_to_inverter(SSID):
         print ("connected to inverter")
         return(0)
     except Exception as error:
-        print("An error occurred:", type(error).__name__)
+        print("An error occurred:", type(error).__name__) 
         return(1)
 
 
 def connect_to_home():
     wdt.feed()
     try:
-        station.active(True)
         print("connecting to IOT Network")
-        station.ifconfig(('192.168.0.189', '255.255.255.0', '192.168.0.1', '192.168.0.2'))
-        station.connect("home_ssid", "home_password")
+        #Found a Static IP works better
+        station.ifconfig(('192.168.0.166', '255.255.255.0', '192.168.0.1', '192.168.0.2'))
+        # connecting/DCing to this wifi with a password seems to be fraight with issues. have a
+        # SSID with MAC filtering which keeps it accessible to this device only. YMMV
+        station.connect("solar_in", "")
         count = 0
         while not station.isconnected():
             sleep(1)
@@ -114,7 +125,7 @@ def connect_to_home():
         print ("connected to home")
         return(0)
     except Exception as error:
-        print("An error occurred:", type(error).__name__)
+        print("An error occurred:", type(error).__name__) 
         return(1)
 
 def disconnect_network():
@@ -123,7 +134,7 @@ def disconnect_network():
         station.disconnect()
         while station.isconnected():
             sleep(1)
-        station.active(False)
+        #station.active(False)
         print('disconnected wifi')
         return(0)
     except:
@@ -138,19 +149,8 @@ def get_solax_data():
         print("Retrieved Data from Inverter")
         return res
     except Exception as error:
-        print("An error occurred:", type(error).__name__)
+        print("An error occurred:", type(error).__name__) 
         return(1)
-
-
-def push_to_node_red(obj):
-    wdt.feed()
-    try:
-        res = post("http://192.168.0.2:1880/update-sensor", headers = {'content-type': 'application/json'}, data = obj)
-        print("Data Pushed to Node-Red")
-        res.close()
-    except Exception as error:
-      print("An error occurred:", type(error).__name__)
-
 
 def calculate_production(tele):
     wdt.feed()
@@ -161,6 +161,31 @@ def calculate_production(tele):
     except Exception as error:
         print("An error occurred:", type(error).__name__)
         return 0
+    
+def post_array_via_mqtt(client_id, broker, topic, array):
+    wdt.feed()
+    try:
+        # Connect to MQTT broker
+        client = MQTTClient(client_id, broker, keepalive=30)
+        client.connect()
+        sleep(1)
+
+        # Convert to byte array
+        payload = bytes(array, 'utf-8')
+
+        # Publish the JSON string to the specified topic
+        client.publish(topic, payload)
+        
+        # Sleep for a bit before disconnecting
+        sleep(5)
+
+        # Disconnect from MQTT broker
+        client.disconnect()
+        del client
+        print("Array posted via MQTT successfully.")
+    except Exception as e:
+        print("Error posting array via MQTT:", str(e))
+
 
 while True:
     arr = []
@@ -181,19 +206,36 @@ while True:
         if con == 0:
             # push the results to node-red
             print("pushing to node-red")
-            for entry in arr:
-                push_to_node_red(entry)
-                sleep(1)
-
             if len(arr) == 2 and type(arr[0]) == str and type(arr[1]) == str: # Calculate total production
                 production = calculate_production(arr[0]) + calculate_production(arr[1])
                 data = {
                     "SN" : "totalproduction",
                     "data": production
                     }
-                push_to_node_red(dumps(data))
+                arr.append(data)
+            post_array_via_mqtt(client_id, broker, topic, dumps(arr))
         else:
             print("Connection Failed - Skipping")
         disconnect_network()
     else:
-        print("nothing to send")
+        # theres a good change both inverters are off. Sleep for 5 minutes, then push an empty value
+        print("nothing to send - sleeping for 5 mins")
+        count = 0
+        while not count == 300:
+            sleep(1)
+            wdt.feed()
+            count = count + 1
+        con = connect_to_home()
+        sleep(3) # give it time to connect
+        if con == 0:
+            # push the results to node-red
+            print("pushing to node-red")
+            data = [{
+                "SN" : "status",
+                "data": "offline"
+                }]
+            post_array_via_mqtt(client_id, broker, topic, dumps(data))
+        else:
+            print("Connection Failed - Skipping")
+        disconnect_network()
+        
